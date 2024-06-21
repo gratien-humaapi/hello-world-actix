1de9607fe258e3201a4614dc5943aaf0f884b22d
Gratien adn <gratien.adn@humaapi.com>
2024-06-21 15:57:22 +0100

    first result

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,124 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
+use std::{fs, sync::{Arc, Mutex}};
+
+use actix_web::{delete, get, post, put, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
 struct AddInfo {
     title: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Clone, Debug)]
 struct Todo {
     id: usize,
     title: String,
 }
 
 impl Todo {
-    fn new(title: String) -> Self {
-        unsafe {
-            let id = HELLO_WORLD.len();
-            Todo { id, title }
-        }
+    fn new(id: usize, title: String) -> Self {
+        Todo { id, title }
     }
 }
 
-static mut HELLO_WORLD: Vec<Todo> = Vec::new();
+struct AppState {
+    tasks: Mutex<Vec<Todo>>,
+}
+
+#[get("/todos")]
+async fn get_tasks(data: web::Data<AppState>) -> impl Responder {
+    let tasks = data.tasks.lock().unwrap();
+    HttpResponse::Ok().json(&*tasks)
+}
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn home() -> impl Responder {
+    let contents = fs::read_to_string("index.html").unwrap();
+
+    HttpResponse::Ok().body(contents)
 }
 
-#[post("/add-task")]
-async fn add_task(req_body: web::Json<AddInfo>) -> impl Responder {
-    unsafe {
-        HELLO_WORLD.push(Todo::new(req_body.title.clone()));
-        println!("body = {:?}", HELLO_WORLD);
+#[get("/todos/{id}")]
+async fn get_task(
+    path: web::Path<usize>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let mut tasks = data.tasks.lock().unwrap();
+    let id: usize = path.into_inner();
+    if let Some(task) = tasks.iter_mut().find(|x| x.id == id) {
+        HttpResponse::Ok().json(task)
+    }else {
+        HttpResponse::NotFound().body(format!("{id} not found"))
     }
-
-    HttpResponse::Ok().body(req_body.title.clone())
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[post("/todos")]
+async fn add_task(data: web::Data<AppState>, req_body: web::Json<AddInfo>) -> impl Responder {
+    let mut tasks = data.tasks.lock().unwrap();
+    let id = tasks.len();
+    let todo = Todo::new(id, req_body.title.to_string());
+    tasks.push(todo.clone());
+    HttpResponse::Ok().json(todo.clone())
+}
+
+#[put("/todos/{id}")]
+async fn update_task(
+    path: web::Path<usize>,
+    data: web::Data<AppState>,
+    req_body: web::Json<AddInfo>,
+) -> impl Responder {
+    let mut tasks = data.tasks.lock().unwrap();
+    let id: usize = path.into_inner();
+    if let Some(task) = tasks.iter_mut().find(|x| x.id == id) {
+        task.title = req_body.title.clone();
+    }
+    HttpResponse::Ok().body(format!("{id} is modified"))
+}
+
+#[delete("/todos/{id}")]
+async fn delete_task(
+    path: web::Path<usize>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let mut tasks = data.tasks.lock().unwrap();
+    let id: usize = path.into_inner();
+    let original_len = tasks.len();
+    tasks.retain(|x| x.id != id);
+    let new_len = tasks.len();
+
+    if new_len < original_len {
+        HttpResponse::Ok().body(format!("{id} is deleted"))
+    } else {
+        HttpResponse::NotFound().body(format!("{id} not found"))
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let app_data = Arc::new(AppState {
+        tasks: Mutex::new(vec![
+            Todo {
+                id: 0,
+                title: "Learn jem".to_string(),
+            },
+            Todo {
+                id: 1,
+                title: "Learn rem".to_string(),
+            },
+            Todo {
+                id: 2,
+                title: "Learn rtt".to_string(),
+            },
+        ]),
+    });
+    HttpServer::new(move || {
         App::new()
-            .service(hello)
+            .app_data(web::Data::from(app_data.clone()))
+            .service(get_tasks)
             .service(add_task)
-            .route("/hey", web::get().to(manual_hello))
+            .service(update_task)
+            .service(delete_task)
+            .service(get_task)
+            .service(home)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
